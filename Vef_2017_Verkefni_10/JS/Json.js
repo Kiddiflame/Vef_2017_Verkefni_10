7b6b59de2688cf2f14d6658f3035ed32b4e73973
Kiddiflame <kiddiflame@gmail.com>
2017-12-13 11:01:24 +0000

    Add files via upload

---
+++ Vef_2017_Verkefni_10/JS/Json.js
@@ -0,0 +1,69 @@
+var concertEvents = [];
+var $search = $("#filter");
+
+	$.ajax({
+	  'url': 'http://apis.is/concerts',
+	  'type': 'GET',
+	  'dataType': 'json',
+	  'success': function(data) {
+	   
+			for(let i = 0; i<data.results.length; i++){
+				concertEvents.push
+				({
+					name:data.results[i].eventDateName,
+					when:data.results[i].dateOfShow,
+					where:data.results[i].eventHallName,
+					imageSrc:data.results[i].imageSource
+				});
+			}
+			console.log(data);
+			console.log(concertEvents);
+			RenderEvent();
+
+		    //console.log(concertEvents[5].name)  
+	  }
+	});
+
+function RenderEvent(){
+	var strResult = "";
+	for (let i = 0; i<concertEvents.length; i++) {
+	 		strResult += "<div id='event_tap'><img src='" + concertEvents[i].imageSrc + 
+	 		"' data-tags =" +concertEvents[i].name+"><div id='event_info'> <b>Nafn:</b> " + concertEvents[i].name + "<br> <b>Staðsetning:</b> " 
+	 		+ concertEvents[i].where + "<br> <b>Tími:</b> " + concertEvents[i].when + "</div></div>";
+	 }
+	 $("#event_list").html(strResult)
+}
+
+/*(function()
+{
+var $events = $('event_tap img');
+var tagged = {};
+
+	$('#filter').keyup(function(){
+   var valThis = $(this).val();
+    $("#even").foreach(function(){
+     var text = $(this).text().toLowerCase();
+        (text.indexOf(valThis) == 0) ? $(this).show() : $(this).hide();         
+   });
+});
+
+
+});*/
+
+
+
+var $events = $('event_tap img');
+var $search = $('#filter');
+
+$('#filter').keyup(function(){
+   var valThis = $(this).val().toLowerCase();
+    if(valThis == ""){
+        $('#event_tap > img').show();
+        console.log("bleh");           
+    } else {
+        $('#event_tap > img').each(function(){
+            var text = $(this).text().toLowerCase();
+            (text.indexOf(valThis) <= -1) ? $(this).show() : $(this).hide();
+        });
+   };
+});
